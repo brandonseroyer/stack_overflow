56e568718eb6bc9cae3d246382f33bd711176c5b
Janice Laset-Parkerson & Brandon Seroyer <student@epicodus.com>
2015-10-20 10:48:41 -0700

    add a counter for daily questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,9 @@ class Question < ActiveRecord::Base
   has_many :answers
 
   validates :question, :presence => true
+
+  def self.daily_questions
+    date = Date.today
+    sum = Question.where(:created_at => date.beginning_of_day..date.end_of_day).count
+  end
 end
