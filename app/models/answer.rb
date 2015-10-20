a7ddd2fedc427bea5cf97184f98277cd921dda1b
Janice Laset-Parkerson & Brandon Seroyer <student@epicodus.com>
2015-10-20 10:51:33 -0700

    add a counter for daily answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,9 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   validates :answer, :presence => true
+
+  def self.daily_answers
+    date = Date.today
+    sum = Answer.where(:created_at => date.beginning_of_day..date.end_of_day).count
+  end
 end
